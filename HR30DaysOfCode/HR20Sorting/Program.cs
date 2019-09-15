b0c2a8e5db152b422055762658016bda7183e8c1
Jim Burnett <jimburnetto@hotmail.com>
2019-09-15 12:26:11 -0500

    bubble sort done

---
+++ HR30DaysOfCode/HR20Sorting/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+        // Write Your Code Here
+        int eFirst = 0;
+        int eLast = 0;
+        int nSwaps = 0;
+        for (int i = 0; i < n; i++)
+        {
+            
+            for (int j = 0; j < n - 1; j++)
+            {
+ 
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1])
+                {
+                    //swap(a[j], a[j + 1]);
+                    int bigger = a[j];
+                    int smaller = a[j + 1];
+                    a[j] = smaller;
+                    a[j + 1] = bigger;
+                    nSwaps++;
+                }
+                if (nSwaps == 0)
+                {
+                  //  break; //array is sorted
+                }
+            }
+        }
+        eFirst = a[0];
+        //eLast = a[n-1];
+        eLast = a.Last();
+        Console.WriteLine($"Array is sorted in {nSwaps} swaps.");
+        //
+        Console.WriteLine($"First Element: {eFirst}");
+        Console.WriteLine($"Last Element: {eLast}");
+
+    }
+}
+
